939ac970bc06c0b4fb386fa7888859b153f2d35a
Roy Cerdan <62484188+Roywesome@users.noreply.github.com>
2022-07-29 01:07:02 -0500

    se greago Face

---
+++ components/Face.js
@@ -0,0 +1,30 @@
+import { View, Text, StyleSheet } from 'react-native';
+import React from 'react';
+import Icon from 'react-native-vector-icons/Fontisto';
+
+
+export default function Face({icon, title, color, full}) {
+  return (
+    <View>
+      { full ? (
+          <View style={{backgroundColor: color, borderRadius: 40}}>
+            <Icon name={icon} size={36} color={'#fff'} />
+          </View>
+      ) : (
+        <Icon name={icon} size={36} color={color} />
+      )}
+       <Text style={[styles.faceText, {color}]}>{title}</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    faceGroup: {
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    faceText: {
+        fontSize: 16,
+        marginTop:6
+    }
+})s
